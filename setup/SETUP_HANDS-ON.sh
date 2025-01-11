014b935fdeca4fcb75bab21b0c5d017aa34e8d44
Toshiharu-Konuma-sti <hoge@1010-00451.>
2025-01-11 17:16:32 +0900

    added the new script for setting up

---
+++ setup/SETUP_HANDS-ON.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+clear
+S_TIME=$(date +%s)
+CUR_DIR=$(cd $(dirname $0); pwd)
+
+echo "############################################################"
+echo "# START SCRIPT"
+echo "############################################################"
+
+$CUR_DIR/step01-install.sh
+
+E_TIME=$(date +%s)
+DURATION=$((E_TIME - S_TIME))
+
+echo "############################################################"
+echo "# FINISH SCRIPT ($DURATION seconds)"
+echo "############################################################"
