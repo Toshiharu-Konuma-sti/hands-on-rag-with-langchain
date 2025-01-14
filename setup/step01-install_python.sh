a7fe1409ca3251250410c5b2bd7778e6976e5934
Toshiharu-Konuma-sti <hoge@1010-00451.>
2025-01-14 14:28:33 +0900

    removed python-is-python3 from packages to install

--- setup/step01-install_python.sh
+++ setup/step01-install_python.sh
@@ -20,7 +20,7 @@ EOS
 	fi
 	pip install virtualenv
 else
-	sudo apt install -y python-is-python3 python3-pip python3-venv
+	sudo apt install -y python3-pip python3-venv
 fi
 
 call_show_finish_banner
